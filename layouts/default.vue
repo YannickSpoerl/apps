d524975fccaf8ea378a0d32d16a546183a2e7038
Yannick Spoerl <spoerlyannick@gmail.com>
2020-07-16 17:52:50 +0200

    Remove unused styl

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,17 +29,6 @@ export default {
 <style lang="stylus">
 @import '~assets/style/index'
 
-.content-wrapper
-  padding 160px 15px 80px 15px
-  min-height calc(100vh - 80px - 60px - 160px)
-  max-width $contentWidth
-  margin 0 auto
-
-  @media (max-width: $MQMobile)
-    &
-      padding 100px 15px 20px 15px
-      min-height calc(100vh - 20px - 60px - 100px)
-
 #vuepress-theme-blog__global-layout
   word-wrap break-word
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="content__default">
     <p>Hello</p>
   </div>
 </template>
