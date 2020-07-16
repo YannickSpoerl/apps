780df8f0d02ac8ca1619621b0cafa99340025e21
Yannick Spoerl <spoerlyannick@gmail.com>
2020-07-16 18:03:31 +0200

    Add nuxtjs/sitemap, add title and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,61 +1,40 @@
 import messages from './assets/internationalization/index.js'
 
 export default {
-  /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
   mode: 'universal',
-  /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
   target: 'static',
-  /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Yannick Spoerl - Apps',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Yannick Spoerl - Apps' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  /*
-  ** Global CSS
-  */
   css: [
   ],
-  /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
   plugins: [
-    '~plugins/loadContent.js'
+    '~plugins/loadContent.js',
+    '~plugins/loadApps.js'
   ],
-  /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
   components: true,
-  /*
-  ** Nuxt.js dev-modules
-  */
   buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module'
   ],
-  /*
-  ** Nuxt.js modules
-  */
   modules: [
-    'nuxt-i18n'
+    'nuxt-i18n',
+    '@nuxtjs/sitemap'
   ],
+  sitemap: {
+    hostname: 'https://apps.yannickspoerl.com',
+    exclude: [
+      '/privacy',
+      '/license'
+    ]
+  },
   i18n: {
     locales: [
       {
@@ -77,10 +56,6 @@ export default {
       alwaysRedirect: true
     }
   },
-  /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
   build: {
   }
 }
