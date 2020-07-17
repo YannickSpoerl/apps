8c08df4069893297219b6f1624b356d0c9b487e9
Yannick Spoerl <spoerlyannick@gmail.com>
2020-07-17 12:32:08 +0200

    Added apps app

--- assets/apps/index.js
+++ assets/apps/index.js
@@ -2,9 +2,10 @@ import laundry from './laundry.json'
 import mensavue from './mensavue.json'
 import blog from './blog.json'
 import wannistalex from './wannistalex.json'
+import appsApp from './apps.json'
 
 const apps = [
-  laundry, blog, wannistalex, mensavue
+  laundry, blog, wannistalex, mensavue, appsApp
 ]
 
 export default apps
